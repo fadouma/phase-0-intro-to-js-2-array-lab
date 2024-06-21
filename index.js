c5845d3a0e5744e047a8e02bdc86bfcbfc9d9efe
fadouma ahmed <fannyahmed85@gmail.com>
2024-06-21 11:04:33 -0600

    complete

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name){
+    cats.push(name)
+    return cats
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+    return cats
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+    return cats
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+    return cats
+}
+function appendCat(name){
+    const fanny = [...cats, "Broom"]
+    return fanny
+}
+function prependCat(name){
+    const fanny = ["Arnold", ...cats]
+    return fanny
+}
+function removeLastCat(){
+    const copy = cats.slice()
+    copy.pop()
+    return copy
+}
+function removeFirstCat(){
+    const copy = cats.slice()
+    copy.shift()
+    return copy
+}
